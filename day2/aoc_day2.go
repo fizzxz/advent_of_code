a7af23e9381f04c59f87802865475a92b074419e
Faisal Kabeer <faisalk96@outlook.com>
2022-12-02 17:52:10 +0000

    Day 2 completed both parts of qeustion

--- day2/aoc_day2.go
+++ day2/aoc_day2.go
@@ -15,24 +15,25 @@ func main() {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	countPoints, lines := 0, 0
+	countPointsStratGuide1 := 0
+	x := 0
 	for scanner.Scan() {
 		scannerTextRemoveSpace := strings.ReplaceAll(scanner.Text(), " ", "")
 		opponent := string(scannerTextRemoveSpace[0:1])
 		me := string(scannerTextRemoveSpace[1:])
-		countPoints += response(opponent, me)
-		lines++
+		countPointsStratGuide1 += responseStratGuide1(opponent, me)
+		x += responseStratGuide2(opponent, me)
 		fmt.Println(scannerTextRemoveSpace)
-
 	}
-	fmt.Println(countPoints)
-	fmt.Print(lines)
+	fmt.Println(countPointsStratGuide1)
+
+	fmt.Println(x)
 }
 
 //A - X = ROCK
 //B - Y = PAPER
 //C - Z = SCISSORS
-func response(opponent, me string) int {
+func responseStratGuide1(opponent, me string) int {
 	switch opponent {
 	case "A":
 		if me == "X" {
@@ -62,6 +63,42 @@ func response(opponent, me string) int {
 			return 0 + 2
 		}
 		if me == "Z" {
+			return 3 + 3
+		}
+	}
+	return 0
+}
+
+func responseStratGuide2(opponent, me string) int {
+	switch opponent {
+	case "A":
+		if me == "Y" {
+			return 3 + 1
+		}
+		if me == "Z" {
+			return 6 + 2
+		}
+		if me == "X" {
+			return 0 + 3
+		}
+	case "B":
+		if me == "X" {
+			return 0 + 1
+		}
+		if me == "Y" {
+			return 3 + 2
+		}
+		if me == "Z" {
+			return 6 + 3
+		}
+	case "C":
+		if me == "Z" {
+			return 6 + 1
+		}
+		if me == "X" {
+			return 0 + 2
+		}
+		if me == "Y" {
 			return 3 + 3
 		}
 	}
